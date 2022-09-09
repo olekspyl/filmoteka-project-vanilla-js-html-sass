e282a0c6cea0f8693b7c49619472de01fc6b7aa2
AndreyKapus <kapustnikov@ukr.net>
2022-09-09 23:51:22 +0300

    modal/render one film card

---
+++ src/modal-film.js
@@ -0,0 +1,63 @@
+const renderModalOneFilm = modalFilm => {
+  const {
+    poster_path,
+    title,
+    id,
+    vote_average,
+    vote_count,
+    popularity,
+    original_title,
+    genres,
+    overview,
+  } = modalFilm;
+
+  const genresList = genres.map(item => item.name).join(', ');
+
+  const markupModalOneFilm = `<div class="film-card__image-block">
+        <img
+          class="film-card__image"
+          src="https://image.tmdb.org/t/p/original${poster_path}"
+          alt="film"
+        />
+      </div>
+      <div class="film-card__description">
+        <h1 class="film-card__description-title">${title}</h1>
+        <ul class="film-card__description-set">
+          <li class="description-set">
+            <p class="description-set__text">Vote / Votes</p>
+            <p class="description-set__value">
+              <span class="description-set__rating">${vote_average}</span>
+              <span class="description-set__separator">/</span>
+              <span class="description-set__vote">${vote_count}</span>
+            </p>
+          </li>
+          <li class="description-set">
+            <p class="description-set__text">Popularity</p>
+            <p class="description-set__value">${popularity}</p>
+          </li>
+          <li class="description-set">
+            <p class="description-set__text">Original Title</p>
+            <p class="description-set__value">${original_title}</p>
+          </li>
+          <li class="description-set">
+            <p class="description-set__text">Genre</p>
+            <p class="description-set__value">${genresList}</p>
+          </li>
+        </ul>
+        <div class="film-card__description-about">
+          <h2 class="description-about__title">About</h2>
+          <p class="description-about__text">${overview}</p>
+        </div>
+        <div class="film-card__description-button">
+          <button class="description-button description-button__watched" data-id="${id}">
+            add to Watched
+          </button>
+          <button class="description-button description-button__queue" data-id="${id}">
+            add to queue
+          </button>
+        </div>
+      </div>
+    </div>`;
+
+  refs.modalFilm.insertAdjacentHTML('beforeend', markupModalOneFilm);
+};
