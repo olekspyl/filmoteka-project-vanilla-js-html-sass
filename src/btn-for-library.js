520a6d67ef046d4e22ac13d51ac7b28f4345f527
BohdanovaYliia <bogdanovaya1989@gmail.com>
2022-09-11 21:52:38 +0300

    Adds function for my library.

---
+++ src/btn-for-library.js
@@ -0,0 +1,35 @@
+import createMarkup from './js/markupForGallery';
+
+let watchedMovies = [];
+let queueMovies = [];
+
+const libraryLink = document.querySelector('.js-my-library');
+const libraryWrap = document.querySelector('.library-list');
+
+libraryLink.addEventListener('click', onLibraryLinkClick);
+
+function onLibraryLinkClick() {
+    libraryWrap.innerHTML = ' ';
+
+    watchedMovies = JSON.parse(localStorage.getItem('watchedMovies'));
+    queueMovies = JSON.parse(localStorage.getItem('queueMovies'));
+    const noWatched = watchedMovies === null;
+    const noQueue = queueMovies === null;
+
+    if (noWatched && noQueue) {
+        const emptyLibrary = `<div class="empty-library"> <img class="empty-library__img" src="images/empty_library.jpg" alt="library is empty" /> <p class="empty-library__title">YOUR LIBRARY IS EMPTY!</p> </div>`;
+        filmotekaWrap.innerHTML = emptyLibrary;
+    }
+
+    if (!noQueue) {
+        renderMarkup(queueMovies);
+    }
+
+    if (!noWatched) { 
+        renderMarkup(watchedMovies);
+    }
+}
+
+function renderMarkup(savedMovies) {
+    libraryWrap.innerHTML= createMarkup(savedMovies);
+}
