b4b6a53e3d729e8e12e68c0ba3f11e91b4ed3930
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-09-11 21:07:14 +0300

    1

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import { console } from './js/example-index';
 import {} from './js/gallery-popular-films';
 import { loader } from './js/loader';
 import './js/teamModal.js';
-import { modal_film } from './modal-film';
+// import { modal_film } from './modal-film';
 import { fetch } from './fetch-movie';
+import { keyMovieQuery } from './js/keyMovieQuery';
