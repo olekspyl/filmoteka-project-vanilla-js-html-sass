7fed5630669640e86f63af709e451e65dd90c503
BohdanovaYliia <bogdanovaya1989@gmail.com>
2022-09-12 18:53:51 +0300

    Adds changes to functions and adds Notiflix.

--- src/btn-in-modal.js
+++ src/btn-in-modal.js
@@ -1,4 +1,5 @@
 import { getMovieById } from './js/fetch-movie';
+import Notiflix from 'notiflix';
 
 let watchedMovies = [];
 let queueMovies = [];
@@ -7,7 +8,7 @@ const modalWrap = document.querySelector('.film-card');
 modalWrap.addEventListener('click', onModalClick);
 
 function onModalClick(evt) {
-    console.log(evt.target);
+    // console.log(evt.target);
 
     if (evt.target.classList.contains('description-button__watched')) {
         onBtnAddToWatchedClick(evt);
@@ -19,50 +20,54 @@ function onModalClick(evt) {
 }
 
 async function onBtnAddToWatchedClick(evt) {
-
     const dataWatched = JSON.parse(localStorage.getItem('watchedMovies'));
     const selectedMovie = await getMovieById(evt.target.dataset.id);
 
     if (dataWatched === null || dataWatched === '[]') {
         watchedMovies.push(selectedMovie);
         localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
+
+        Notiflix.Notify.success('This movie added to Watched.');
     }
     else {
         watchedMovies = dataWatched;
         for (let i = 0; i < watchedMovies.length; i += 1) {
 
             if (watchedMovies[i].id === selectedMovie.id) {
-                return;
+                return Notiflix.Notify.failure('This movie has already been added to Watched.');
             }
         }
         watchedMovies.push(selectedMovie);
         localStorage.setItem('watchedMovies', JSON.stringify(watchedMovies));
+
+        Notiflix.Notify.success('This movie added to Watched.');
     }
 }
 
 async function onBtnAddToQueueClick(evt) {
-
     const dataQueue = JSON.parse(localStorage.getItem('queueMovies'));
     const selectedMovie = await getMovieById(evt.target.dataset.id);
 
     if (dataQueue === null || dataQueue === '[]') {
         queueMovies.push(selectedMovie);
         localStorage.setItem('queueMovies', JSON.stringify(queueMovies));
+
+        
+        Notiflix.Notify.success('This movie added to Queue.');
     }
     else {
         queueMovies = dataQueue;
         for (let i = 0; i < queueMovies.length; i += 1) {
 
             if (queueMovies[i].id === selectedMovie.id) {
-                return;
+                
+                return Notiflix.Notify.failure('This movie has already been added to Queue.');
             }
         }
 
         queueMovies.push(selectedMovie);
         localStorage.setItem('queueMovies', JSON.stringify(watchedMovies));
-    }
-}
 
-function changeBtnAddToWatched() {
-    
+        Notiflix.Notify.success('This movie added to Queue.');
+    }
 }
