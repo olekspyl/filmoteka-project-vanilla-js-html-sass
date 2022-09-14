b38c6025e22312ff7570af5fd666611aad439779
BohdanovaYliia <bogdanovaya1989@gmail.com>
2022-09-14 20:37:26 +0300

    Adds changes2

--- src/js/gallery-popular-films.js
+++ src/js/gallery-popular-films.js
@@ -27,14 +27,15 @@ export async function onGalleryClick(e) {
   const filmInfo = await getMovieById(e.currentTarget.id);
   renderModalOneFilm(filmInfo);
   onOpenModal();
-  
+
+  const modalWatchedBtn = document.querySelector('.description-button__watched');
+  const modalQueueBtn = document.querySelector('.description-button__queue');
+
   const watchedMovies = JSON.parse(localStorage.getItem('watchedMovies'));
   const queueMovies = JSON.parse(localStorage.getItem('queueMovies'));
 
   for (let i = 0; i < watchedMovies.length; i += 1) {
     if (watchedMovies[i].id === filmInfo.id) {
-      const modalWatchedBtn = document.querySelector('.description-button__watched');
-
       modalWatchedBtn.textContent = 'remove from watched';
       modalWatchedBtn.classList.remove('description-button__watched');
       modalWatchedBtn.classList.add('remove-button__watched');
@@ -43,8 +44,6 @@ export async function onGalleryClick(e) {
 
   for (let i = 0; i < queueMovies.length; i += 1) {
     if (queueMovies[i].id === filmInfo.id) {
-      const modalQueueBtn = document.querySelector('.description-button__queue');
-
       modalQueueBtn.textContent = 'remove from queue';
       modalQueueBtn.classList.remove('description-button__queue');
       modalQueueBtn.classList.add('remove-button__queue');
